61ca64adbe932fd83d0dcd2a1faa48f18fdd3986
jagrutiburde0413 <124602129+jagrutiburde0413@users.noreply.github.com>
2023-04-01 19:07:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+var promise=new Promise(function(reslove){
+	setTimeOut(function(){
+		if(isValid()){
+			resolve("correct")
+		}else{
+			reject("invalid User")
+		}
+	},1000)
+})
+
+function isValid(){
+	return true;
+}
